08d3294f1c478f952e1782478cc0438e4ea42904
Kara <kara_himanshu@yahoo.com>
2025-04-27 00:43:22 +0530

    Added brute force

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,3 +1,15 @@
+## Brute Force
+class Solution:
+    def maxSubArray(self, nums):
+        ans = -inf
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                ans = max(ans, cur_sum)
+        return ans
+
+## Kadane's Algorithm
 arr = [-2,1,-3,4,-1,2,1,-5,4]
 maxSub = arr[0]
 curSum = 0
