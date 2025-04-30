43e66bac9a02672cea6373b4dfef8fc4de076206
Kara <kara_himanshu@yahoo.com>
2025-04-30 16:16:56 +0530

    First and Last Occurance of an element

---
+++ BS/FirstLastOccurance.py
@@ -0,0 +1,39 @@
+def firstOccurance(arr,target):
+
+    start = 0
+    end = len(arr) -1
+    res = -1
+    while start <= end:
+        #  mid = (start+end) // 2
+         mid = start + (end - start) // 2
+         if arr[mid] == target:
+             res = mid
+             end = mid - 1
+         elif arr[mid] > target:
+            end = mid - 1
+         else:
+            start = mid + 1
+    return res
+
+def lastOccurance(arr,target):
+
+    start = 0
+    end = len(arr) -1
+    res = -1
+    while start <= end:
+        #  mid = (start+end) // 2
+         mid = start + (end - start) // 2
+         if arr[mid] == target:
+             res = mid
+             start = mid + 1
+         elif arr[mid] > target:
+            end = mid - 1
+         else:
+            start = mid + 1
+    return res
+
+arr = [3,4,10,10,10,12,13,15]
+target = 10
+
+print(firstOccurance(arr, target))
+print(lastOccurance(arr, target))
